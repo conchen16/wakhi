9f9a32535808407649dbf14b2f25a1a59918c569
Connie Chen <conchen16@gmail.com>
2021-02-12 22:18:57 -0800

    final L6_3 Lab 6 version

--- L6_3/tsdb/skeletons/Index.lisp
+++ L6_3/tsdb/skeletons/Index.lisp
@@ -23,5 +23,5 @@
 ((:path . "matrix") (:content . "matrix: A test suite created automatically from the test sentences given in the Grammar Matrix questionnaire."))
 ((:path . "corpus") (:content . "Wakhi"))
 ; New test suites here. For example:
-; ((:path . "new-test-suite") (:content . "New Test Suite: A description of the new test suite located at the subdirectory new-test-suite."))
+((:path . "lab6") (:content . "test suite for lab 6."))
 )
